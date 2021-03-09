ead001d0cb540b748993716b5d4c03faa3563cb1
l-Il <gerkamano@gmail.com>
2021-03-09 16:59:24 +0300

    added OOP

--- main.py
+++ main.py
@@ -2,49 +2,49 @@ from datetime import datetime
 from time import sleep
 from termcolor import colored
 
-A = [[0] * 6 for i in range(4)]
+
+class Time:
+    def __init__(self):
+        self.A = [[0] * 6 for i in range(4)]
+        self.output()
+
+    def binary(self, i, column):
+        if i == 0:
+            self.A[0][column], self.A[1][column], self.A[2][column], self.A[3][column] = 0, 0, 0, 0
+        elif i == 1:
+            self.A[0][column], self.A[1][column], self.A[2][column], self.A[3][column] = 0, 0, 0, 1
+        elif i == 2:
+            self.A[0][column], self.A[1][column], self.A[2][column], self.A[3][column] = 0, 0, 1, 0
+        elif i == 3:
+            self.A[0][column], self.A[1][column], self.A[2][column], self.A[3][column] = 0, 0, 1, 1
+        elif i == 4:
+            self.A[0][column], self.A[1][column], self.A[2][column], self.A[3][column] = 0, 1, 0, 0
+        elif i == 5:
+            self.A[0][column], self.A[1][column], self.A[2][column], self.A[3][column] = 0, 1, 0, 1
+        elif i == 6:
+            self.A[0][column], self.A[1][column], self.A[2][column], self.A[3][column] = 0, 1, 1, 0
+        elif i == 7:
+            self.A[0][column], self.A[1][column], self.A[2][column], self.A[3][column] = 0, 1, 1, 1
+        elif i == 8:
+            self.A[0][column], self.A[1][column], self.A[2][column], self.A[3][column] = 1, 0, 0, 0
+        elif i == 9:
+            self.A[0][column], self.A[1][column], self.A[2][column], self.A[3][column] = 1, 0, 0, 1
+
+    def output(self):
+        while True:
+            self.binary(datetime.now().hour // 10, 0)
+            self.binary(datetime.now().hour % 10, 1)
+            self.binary(datetime.now().minute // 10, 2)
+            self.binary(datetime.now().minute % 10, 3)
+            self.binary(datetime.now().second // 10, 4)
+            self.binary(datetime.now().second % 10, 5)
+            print('   ', colored(f'{datetime.now().hour // 10}{datetime.now().hour % 10}:{datetime.now().minute // 10}{datetime.now().minute % 10}:{datetime.now().second // 10}{datetime.now().second % 10}','cyan'))
+            for i in range(4):
+                for j in range(6):
+                    print(f'{self.A[i][j]} ', end=' ')
+                print()
+            sleep(1)
+            print()
 
 
-def output():
-    for i in range(4):
-        for j in range(6):
-            print(f'{A[i][j]} ', end=' ')
-        print()
-
-
-def binary(i, column):
-    if i == 0:
-        A[0][column], A[1][column], A[2][column], A[3][column] = 0, 0, 0, 0
-    elif i == 1:
-        A[0][column], A[1][column], A[2][column], A[3][column] = 0, 0, 0, 1
-    elif i == 2:
-        A[0][column], A[1][column], A[2][column], A[3][column] = 0, 0, 1, 0
-    elif i == 3:
-        A[0][column], A[1][column], A[2][column], A[3][column] = 0, 0, 1, 1
-    elif i == 4:
-        A[0][column], A[1][column], A[2][column], A[3][column] = 0, 1, 0, 0
-    elif i == 5:
-        A[0][column], A[1][column], A[2][column], A[3][column] = 0, 1, 0, 1
-    elif i == 6:
-        A[0][column], A[1][column], A[2][column], A[3][column] = 0, 1, 1, 0
-    elif i == 7:
-        A[0][column], A[1][column], A[2][column], A[3][column] = 0, 1, 1, 1
-    elif i == 8:
-        A[0][column], A[1][column], A[2][column], A[3][column] = 1, 0, 0, 0
-    elif i == 9:
-        A[0][column], A[1][column], A[2][column], A[3][column] = 1, 0, 0, 1
-
-
-while True:
-    print('   ', colored(
-        f'{datetime.now().hour // 10}{datetime.now().hour % 10}:{datetime.now().minute // 10}{datetime.now().minute % 10}:{datetime.now().second // 10}{datetime.now().second % 10}',
-        'cyan'))
-    binary(datetime.now().hour // 10, 0)
-    binary(datetime.now().hour % 10, 1)
-    binary(datetime.now().minute // 10, 2)
-    binary(datetime.now().minute % 10, 3)
-    binary(datetime.now().second // 10, 4)
-    binary(datetime.now().second % 10, 5)
-    output()
-    sleep(1)
-    print()
+_ = Time()
